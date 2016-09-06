5f325f9f63eaf9ef15c861f5694a9e210a883638
Sara <sarah.alalaya@gmail.com>
2016-09-06 17:16:31 +0300

    Fizzbuzzing

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,11 +5,9 @@ public class FizzBuzz {
   }
 
   public String getResult(int number) {
-    if (number % 3 == 0 ){
-      return ("Fizz");
-    } else if (number % 5 == 0 ){
-      return ("Buzz");
-    }
+    if (number%15 == 0) return "FizzBuzz";
+    if (number%3 == 0)  return "Fizz";
+    if (number%5 == 0)  return "Buzz";
     return ("Nothing");
   }
 
--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -2,15 +2,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+  
+  FizzBuzz fizzBuzz = new FizzBuzz();
+
   @Test
-  public void getResultSgouldReturnFizzIfTheNumberIsDivisibalBy3() {
-    FizzBuzz fizzBuzz = new FizzBuzz();
+  public void getResultShouldReturnFizzIfTheNumberIsDivisibalBy3() {
     assertEquals("Fizz", fizzBuzz.getResult(3));
   }
 
   @Test
   public void getResultShouldReturnBuzzIfTheNumberIsDivisibleBy5() {
-    FizzBuzz fizzBuzz = new FizzBuzz();
     assertEquals("Buzz", fizzBuzz.getResult(5));
+  }
+
+  @Test
+  public void getResultShouldReturnFizzBuzzIfTheNumberIsDivisibleBy15() {
+    assertEquals("FizzBuzz", fizzBuzz.getResult(15));
   }
 }
